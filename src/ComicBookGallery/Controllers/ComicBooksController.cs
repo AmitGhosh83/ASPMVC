91f1509b40d81f72a04965e3e851d9698f1712ca
Amit Kumar Ghosh <amitghosh83@gmail.com>
2017-05-22 14:03:52 -0700

    Added Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,10 +8,10 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController: Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return ("Hello from the Comic Book Controller");
+            return View();
         }
     }
 
-}
+} 
